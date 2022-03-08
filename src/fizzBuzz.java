0bbe85af0e4bf27de0c187bf1395c4d384432690
Yooshly882 <59481667+Yooshly882@users.noreply.github.com>
2022-03-07 22:50:53 -0500

    First Commit
    First Commit! I'm trying to remember the rules for fallthrough here. This isn't working quite right; what happens is when the first if statement runs, it also runs the else statement--but when the second statement runs, it ignores the else case.
    I think it's that in this arrangement, two consecutive if statements followed by an else, the final if and the else statements are considered a pair.

---
+++ src/fizzBuzz.java
@@ -0,0 +1,24 @@
+
+public class fizzBuzz {
+	
+	/*I chose to make the start and end global constants, for an easy means of swapping
+	 *the start and end of the loop.
+	 */
+final static int START = 1;
+final static int END = 100;
+
+	public static void main(String[] args) {
+		for (int i = START; i <= END; i++) {
+			if (i % 3 == 0) {
+				System.out.print("Fizz");
+			}
+			if (i % 5 == 0) {
+				System.out.print("Buzz");
+			}
+			else {
+				System.out.print(i);
+			}
+			System.out.print(", ");
+		}
+	}
+}
