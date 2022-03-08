f1876aad33d6f810e4a54fbb66640ee89c5a0209
Yooshly882 <59481667+Yooshly882@users.noreply.github.com>
2022-03-07 23:13:11 -0500

    Update fizzBuzz.java
    Not my *favorite* solution in terms of proper coding practices, but I do feel pretty clever for this one!
    >The outer if/else statement gets the "special cases" check out of the way early--if nothing else, it makes it easier to read. If the number is not divisible by either 3 or 5, just print the integer and move on.
    >The inner if, if statement--while I'm sure is probably not best coding practices--DOES allow the program to check for Fizz, then Buzz, without moving on too soon, with minimal extra language and effort. This means that numbers divisible by only 3 or 5 are dealt with correctly, AND numbers divisible by 3 & 5 (/15) result in "FizzBuzz".

--- src/fizzBuzz.java
+++ src/fizzBuzz.java
@@ -9,11 +9,13 @@ final static int END = 100;
 
 	public static void main(String[] args) {
 		for (int i = START; i <= END; i++) {
-			if (i % 3 == 0) {
-				System.out.print("Fizz");
-			}
-			else if (i % 5 == 0) {
-				System.out.print("Buzz");
+			if ((i % 3 == 0) || (i % 5 == 0)) {
+				if (i % 3 == 0) {
+					System.out.print("Fizz");
+				}
+				if (i % 5 == 0) {
+					System.out.print("Buzz");
+				}
 			}
 			else {
 				System.out.print(i);
