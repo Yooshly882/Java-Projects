1711254f22997c6939d5e98cba12e4ca7da9aef0
Yooshly882 <59481667+Yooshly882@users.noreply.github.com>
2022-03-12 11:14:26 -0500

    Building conway.java, still runtime issues
    I'm running into an issue with running the file in order to test it--I strongly believe this is an ongoing issue with how I'm classifying and saving the files. I'm saving the work I've done here so that I can pull it back if need be, after I finish fiddling around again with my repository's organization.

--- Conway's Game of Life/src/conway.java
+++ Conway's Game of Life/src/conway.java
@@ -1,4 +1,31 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 
-public class conway {
-	
+public class conway extends Application {
+	final int EDGECELLS = 20;
+
+	@Override
+	public void start(Stage stage) {
+		GridPane gridpane = new GridPane();
+		Rectangle cell = new Rectangle();
+
+		for (int i = 0; i < EDGECELLS; i++) {
+			for (int j = 0; j < EDGECELLS; j++) {
+				gridpane.add(cell, i, j);
+			}
+		}
+
+		Scene scene = new Scene(gridpane, 400, 400);
+		stage.setTitle("Conway's Game of Life");
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
 }
